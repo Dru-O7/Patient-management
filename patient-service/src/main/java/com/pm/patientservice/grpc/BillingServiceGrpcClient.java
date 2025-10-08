1028de15a19d4fb1240e99e88997e2c9f913b4e8
Dhruv <96690913+Dru-O7@users.noreply.github.com>
2025-10-08 14:55:57 +0530

    Initial commit: patient and billing services

---
+++ patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
@@ -0,0 +1,17 @@
+package com.pm.patientservice.grpc;
+
+import billing.BillingServiceGrpc;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BillingServiceGrpcClient {
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+
+    public BillingServiceGrpcClient(@Value("${billing.service.address:localhost}") String serverAddress,
+                                    @Value("${billing.service.grpc:9001}") int serverPort
+                                    ){
+
+    }
+
+}
